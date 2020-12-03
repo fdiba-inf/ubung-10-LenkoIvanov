dcc24a45c85d5c241387f4b3fef59c70234a932c
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-12-03 07:53:56 +0000

    sideschange

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -11,23 +11,23 @@ public class Ellipse extends Figure {
     }
 
     public Ellipse(Ellipse otherEllipse) {
-        super(otherEllipse.startPoint, otherEllipse.side2,
-        otherEllipse.side1);
+        super(otherEllipse.startPoint, otherEllipse.side1,
+        otherEllipse.side2);
     }
 
     @Override
     public double calculatePerimeter() {
-        return Math.PI * (3.0 * (side2 + side1) - Math.sqrt((3.0 * side2 + side1) * (side2 + 3.0 * side1)));
+        return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
 
     @Override
     public double calculateArea() {
-        return Math.PI * side2 * side1;
+        return Math.PI * side1 * side2;
     }
 
     @Override
     public String getType() {
-        return (side2 == side1) ? "Circle" : "Ellipse";
+        return (side1 == side2) ? "Circle" : "Ellipse";
     }
 
     @Override
@@ -42,7 +42,7 @@ public class Ellipse extends Figure {
 
     @Override
     public boolean containsClick(Point click) {
-        double solution = Math.pow((click.getX() - startPoint.getX()),2)/(side2 * side2) + Math.pow((click.getY() - startPoint.getY()),2)/(side1 * side1);
+        double solution = Math.pow((click.getX() - startPoint.getX()),2)/(side1 * side1) + Math.pow((click.getY() - startPoint.getY()),2)/(side2 * side2);
 
         if(solution <= 1){
           return true;
